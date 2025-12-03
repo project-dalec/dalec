4803fb9b77a829fbacc119c1bc0d1cbcc9ffc696
Brian Goff <cpuguy83@gmail.com>
2025-12-02 16:15:13 -0800

    Remove unused stuff from old testrunner
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- frontend/test_runner.go
+++ frontend/test_runner.go
@@ -2,13 +2,10 @@ package frontend
 
 import (
 	"context"
-	stderrors "errors"
 	"strconv"
-	"sync"
 
 	"github.com/moby/buildkit/client/llb"
 	gwclient "github.com/moby/buildkit/frontend/gateway/client"
-	"github.com/moby/buildkit/solver/errdefs"
 	"github.com/pkg/errors"
 	"github.com/project-dalec/dalec"
 	"github.com/project-dalec/dalec/internal/testrunner"
@@ -49,40 +46,4 @@ func RunTests(ctx context.Context, client gwclient.Client, sOpt dalec.SourceOpts
 		runTests := testrunner.WithTests(target, frontendSt, sOpt, withTestDeps, tests, opts...)
 		return in.With(runTests).With(testrunner.WithFinalState(frontendSt, in))
 	}
-}
-
-func wrapWithSource(err error, src *errdefs.Source) error {
-	if src != nil {
-		err = errors.Wrapf(err, "%s:%d", src.Info.Filename, src.Ranges[0].Start.Line)
-	}
-	return errdefs.WithSource(err, src)
-}
-
-type errGroup struct {
-	group sync.WaitGroup
-	mu    sync.Mutex
-	errs  []error
-}
-
-func (g *errGroup) Go(f func() error) {
-	g.group.Add(1)
-
-	go func() {
-		defer g.group.Done()
-		err := f()
-		g.mu.Lock()
-		g.errs = append(g.errs, err)
-		g.mu.Unlock()
-	}()
-}
-
-func (g *errGroup) Wait() error {
-	g.group.Wait()
-	g.mu.Lock()
-	defer g.mu.Unlock()
-
-	err := stderrors.Join(g.errs...)
-
-	g.errs = g.errs[:0]
-	return err
 }
