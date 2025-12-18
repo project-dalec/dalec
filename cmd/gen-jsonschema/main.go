97d01d3e62ff094567a3d6aa1d4233bc6b4706de
ashu8912 <aghildiyal@microsoft.com>
2025-12-18 17:20:18 +0530

    frontend: Add json schema subrequest for dalec specs
    Signed-off-by: ashu8912 <aghildiyal@microsoft.com>

--- cmd/gen-jsonschema/main.go
+++ cmd/gen-jsonschema/main.go
@@ -5,22 +5,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"
 	"slices"
 	"strings"
 
-	"github.com/project-dalec/dalec"
 	"github.com/atombender/go-jsonschema/pkg/schemas"
 	"github.com/invopop/jsonschema"
+	"github.com/project-dalec/dalec"
 )
 
 func main() {
-	var r jsonschema.Reflector
-	if err := r.AddGoComments("github.com/project-dalec/dalec", "./"); err != nil {
+	dt, err := generateJSONSchema()
+	if err != nil {
 		panic(err)
 	}
 
+	if len(os.Args) > 1 {
+		// Write to file
+		if err := os.WriteFile(os.Args[1], dt, 0644); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	fmt.Println(string(dt))
+}
+
+// generateJSONSchema generates and returns the JSON schema for Dalec specs.
+// This schema can be used by editors and tools for validation and autocomplete.
+func generateJSONSchema() ([]byte, error) {
+	var r jsonschema.Reflector
+	if err := r.AddGoComments("github.com/project-dalec/dalec", "./"); err != nil {
+		return nil, err
+	}
+
 	schema := r.Reflect(&dalec.Spec{})
+
 	if schema.PatternProperties == nil {
 		schema.PatternProperties = make(map[string]*jsonschema.Schema)
 	}
@@ -28,7 +47,7 @@ func main() {
 
 	dt, err := json.Marshal(schema)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// The above library used is good for generating the schema from the go types,
@@ -36,7 +55,6 @@ func main() {
 	// since the data is not represented in go correctly.
 	// So we convert the schema to JSON and then back to another go type that is more
 	// suitable for manipulation.
-	//
 	// Specifically, the problem with the above library is that the `Type` parameter
 	// is a string, but it should be a []string.
 	// Both are apparently(?) valid jsonschema, but the latter is what we need to
@@ -44,7 +62,7 @@ func main() {
 
 	schema2, err := schemas.FromJSONReader(bytes.NewReader(dt))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	const (
@@ -52,12 +70,16 @@ func main() {
 		argsKey  = "args"
 		buildKey = "build"
 	)
+
 	spec := schema2.Definitions[specKey]
-	spec.Properties[argsKey].AdditionalProperties.Type = append(spec.Properties[argsKey].AdditionalProperties.Type, "integer")
+	// Allow args values to be integers (in addition to strings)
+	spec.Properties[argsKey].AdditionalProperties.Type = append(
+		spec.Properties[argsKey].AdditionalProperties.Type, "integer")
 
 	build := spec.Properties[buildKey]
 	buildType := strings.TrimPrefix(build.Ref, "#/$defs/")
 	build = schema2.Definitions[buildType]
+	// Allow env values to be integers (in addition to strings)
 	build.Properties["env"].Type = append(build.Properties["env"].Type, "integer")
 
 	for _, v := range schema2.Definitions {
@@ -66,30 +88,25 @@ func main() {
 
 	dt, err = json.MarshalIndent(schema2, "", "\t")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	if len(os.Args) > 1 {
-		if err := os.MkdirAll(filepath.Dir(os.Args[1]), 0755); err != nil {
-			panic(err)
-		}
-		if err := os.WriteFile(os.Args[1], dt, 0644); err != nil {
-			panic(err)
-		}
-		return
-	}
-	fmt.Println(string(dt))
+	return dt, nil
 }
 
+// setObjectAllowNull recursively walks the schema and adds "null" as an allowed
+// type for any non-required field that is an object or string.
 func setObjectAllowNull(t *schemas.Type) {
 	if t == nil {
-		panic("nil type")
+		return
 	}
+
 	if t.AdditionalProperties != nil {
 		setObjectAllowNull(t.AdditionalProperties)
 	}
 
 	for k, v := range t.Properties {
+		// Skip required fields - they can't be null
 		if slices.Contains(t.Required, k) {
 			continue
 		}
@@ -97,6 +114,7 @@ func setObjectAllowNull(t *schemas.Type) {
 		t.Properties[k] = v
 	}
 
+	// Check if this type should allow null
 	ok := slices.ContainsFunc(t.Type, func(v string) bool {
 		if v == "null" {
 			// Already allows null, nothing to do.
@@ -109,5 +127,6 @@ func setObjectAllowNull(t *schemas.Type) {
 		return
 	}
 
+	// Add "null" as an allowed type
 	t.Type = append(t.Type, "null")
 }
