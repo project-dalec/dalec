97b18c3096ef7e356eb82716356db0164707046d
Brian Goff <cpuguy83@gmail.com>
2026-06-26 16:45:40 -0700

    Use a valid email in the dummy deb changelog author
    dpkg-buildpackage in Ubuntu 26.04 rejects an empty maintainer email
    address, failing source-package builds with exit code 25. Give the
    fallback changelog author a valid placeholder email so packages without
    an explicit changelog still build on newer dpkg.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- packaging/linux/deb/template_changelog.go
+++ packaging/linux/deb/template_changelog.go
@@ -50,7 +50,7 @@ type changelogWrapper struct {
 
 var dummyChangelogEntry = dalec.ChangelogEntry{
 	Date:   dalec.Date{Time: time.Unix(0, 0)},
-	Author: "Dalec Dummy Changelog <>",
+	Author: "Dalec Dummy Changelog <dalec@users.noreply.github.com>",
 	Changes: []string{
 		"Dummy changelog entry",
 	},
